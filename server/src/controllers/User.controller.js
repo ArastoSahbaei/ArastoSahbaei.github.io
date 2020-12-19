e9c8f20cde698049d9eb53f0d8b37000492e599b
arasto <Arasto.Sahbaei@gmail.com>
2020-12-19 15:50:16 +0100

    created crud operations

--- server/src/controllers/User.controller.js
+++ server/src/controllers/User.controller.js
@@ -1,1 +1,88 @@
 import UserModel from '../models/User.model.js'
+import StatusCode from '../../configurations/StatusCode'
+
+const registerNewUser = async (request, response) => {
+	const user = new UserModel({
+		username: request.body.username,
+		password: request.body.password
+	})
+
+	try {
+		const databaseResponse = await user.save()
+		response.status(StatusCode.CREATED).send(databaseResponse)
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({ message: error.message })
+	}
+}
+
+const getAllUsers = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.find()
+		response.status(StatusCode.OK).send(databaseResponse)
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({ message: error.message })
+	}
+}
+
+const getUserWithID = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.findById(request.params.userId)
+		response.status(StatusCode.OK).send(databaseResponse)
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: 'Error occured while trying to retrieve user with ID: ' + request.params.userId,
+			error: error.message
+		})
+	}
+}
+
+const getUserWithQuery = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.find({ username: request.query.username })
+		databaseResponse.length !== 0
+			? response.status(StatusCode.OK).send(databaseResponse)
+			: response.status(StatusCode.NOT_FOUND).send({ message: 'Could not find user with username ' + request.query.username })
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: 'Error occured while trying to retrieve user with username: ' + request.query.userId,
+			error: error.message
+		})
+	}
+}
+
+const updateValuesOfExistingUser = async (request, response) => {
+	try {
+		if (!request.body) { return response.status(StatusCode.BAD_REQUEST).send({ message: 'Empty values were sent' }) }
+		const databaseResponse = await UserModel.findByIdAndUpdate(request.params.userId, {
+			username: request.body.username,
+			password: request.body.password
+		}, { new: true })
+		response.status(StatusCode.OK).send(databaseResponse)
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: 'Error occured while trying to update values of the user with ID: ' + request.params.userId,
+			error: error.message
+		})
+	}
+}
+
+const deleteUserWithId = async (request, response) => {
+	try {
+		const databaseResponse = await UserModel.findByIdAndDelete(request.params.userId)
+		response.status(StatusCode.OK).send({ message: `Sucessfully deleted the USER with username: ${databaseResponse.username} and ID: ${request.params.userId}` })
+	} catch (error) {
+		response.status(StatusCode.INTERNAL_SERVER_ERROR).send({
+			message: `Error occured while trying to delete user with the ID: ${request.params.userId}`,
+			error: error.message
+		})
+	}
+}
+
+export default {
+	registerNewUser,
+	getAllUsers,
+	getUserWithID,
+	getUserWithQuery,
+	updateValuesOfExistingUser,
+	deleteUserWithId
+}
---
+++ server/src/routes/User.routes.js
@@ -0,0 +1,12 @@
+import UserController from '../controllers/User.controller.js'
+
+const routes = application => {
+	application.post('/user', UserController.registerNewUser)
+	application.get('/user', UserController.getAllUsers)
+	application.get('/user/:userId', UserController.getUserWithID)
+	application.get('/searchuser', UserController.getUserWithQuery)
+	application.put('/user/:userId', UserController.updateValuesOfExistingUser)
+	application.delete('/user/:userId', UserController.deleteUserWithId)
+}
+
+export default routes
