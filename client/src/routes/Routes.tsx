269b0374f9cb114c5970c6008d8ae929f67107d6
arasto <Arasto.Sahbaei@gmail.com>
2020-12-28 21:21:26 +0100

    m

--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -9,18 +9,27 @@ import APIService from '../shared/api/service/APIService'
 
 export const Routes = (props: { children?: React.ReactChild }) => {
 	const { children } = props
-	const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext)
+	const [, setAuthenticatedUser] = useContext(UserContext)
+
+	const isTokenValid = (token: number) => {
+		const cTs = Math.floor(Date.now() / 1000)
+		return (token >= cTs)
+	}
 
 	const parseJWT = async (token: any) => {
 		if (!token) { return }
 		const base64Url = token.split('.')[1]
 		const base64 = base64Url.replace('-', '+').replace('_', '/')
 		const JWT = JSON.parse(window.atob(base64))
-		/* TODO: There has to be a better way to recieve the username? 
-			You cannot just do a getUserWithID like this?
-		*/
-		const response = await APIService.getUserWithID(JWT.id)
-		setAuthenticatedUser({ authenticated: true, id: JWT.id, username: response.data.username })
+
+		if (isTokenValid(JWT.exp)) {
+			/* TODO: There has to be a better way to recieve the username? 
+				You cannot just do a getUserWithID like this? */
+			const response = await APIService.getUserWithID(JWT.id)
+			setAuthenticatedUser({ authenticated: true, id: JWT.id, username: response.data.username })
+		} else {
+			setAuthenticatedUser({ authenticated: false, id: undefined, username: undefined })
+		}
 	}
 
 	useEffect(() => {
