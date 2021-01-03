9706bc91b52c477bc1775f149ecff1e0109da325
arasto <Arasto.Sahbaei@gmail.com>
2021-01-03 14:47:31 +0100

    m

--- client/src/view/UserSettingsView.tsx
+++ client/src/view/UserSettingsView.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const UserSettingsView: React.FC = (): JSX.Element => {
+	const [data, setData] = useState({})
+
 	return (
 		<div>
 			<h1>This is the userSettingsView</h1>
 			<h1>Change password here:</h1>
+			<input placeholder='current password' /> <br />
+			<input placeholder='new password' /> <br />
+			<button onClick={() => console.log('')}>OK</button>
 		</div>
 	)
 }
