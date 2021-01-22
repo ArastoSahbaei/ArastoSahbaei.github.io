cbbfae0e3c95704dff54dd2ec52057a504922205
arasto <Arasto.Sahbaei@gmail.com>
2021-01-22 14:56:47 +0100

    m

--- server/src/models/ShoppingCart.model.js
+++ server/src/models/ShoppingCart.model.js
@@ -6,7 +6,7 @@ const shoppingCart = Schema({
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
 		ref: 'user',
-		/* required: true */
+		required: true
 	},
 	products: [{
 		type: mongoose.Schema.Types.ObjectId,
