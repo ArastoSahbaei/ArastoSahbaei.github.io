0db1b3276e5e9d74ec3df1dcc90a5e8190e4c795
arasto <Arasto.Sahbaei@gmail.com>
2021-01-26 12:44:58 +0100

    M

--- server/src/routes/ShoppingCart.routes.js
+++ server/src/routes/ShoppingCart.routes.js
@@ -1,7 +1,7 @@
 import ShoppingCartController from '../controllers/ShoppingCart.controller.js'
 
 const routes = application => {
-	application.post('/shoppingcart/add', ShoppingCartController.addProduct)
+	/* application.post('/shoppingcart/add', ShoppingCartController.addProduct) */
 	application.get('/shoppingcart', ShoppingCartController.getShoppingCart)
 }
 
