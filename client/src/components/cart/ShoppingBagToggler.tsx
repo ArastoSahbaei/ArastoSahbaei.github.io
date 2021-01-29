158f5da50c8956eb7f0ed119599877f6d9750607
arasto <Arasto.Sahbaei@gmail.com>
2021-01-29 16:25:23 +0100

    remove from cart

--- client/src/components/cart/ShoppingBagToggler.tsx
+++ client/src/components/cart/ShoppingBagToggler.tsx
@@ -9,18 +9,24 @@ export const ShoppingBagToggler = (props: { isShoppingBagOpen: boolean, setIsSho
 	const { isShoppingBagOpen, setIsShoppingBagOpen } = props
 	const [authenticatedUser, setAuthenticatedUser] = useContext<any>(UserContext)
 
-
-
 	const checkout = () => {
 		history.push(RoutingPath.checkoutView)
 		setIsShoppingBagOpen(false)
+	}
+
+	const removeProductFromCart = () => {
+		console.log('')
 	}
 
 	return (
 		<div className={isShoppingBagOpen ? 'cart-drawer open' : 'cart-drawer'}>
 			<h1 onClick={() => setIsShoppingBagOpen(false)}>Exit</h1>
 			<ul>
-				{authenticatedUser?.shoppingCart[0]?.products?.map((product: any, index: number) => <li key={index}> {product} </li>)}
+				{authenticatedUser?.shoppingCart[0]?.products?.map((product: any, index: number) =>
+					<div key={index}>
+						<li onClick={() => removeProductFromCart()}> {product} </li>
+					</div>
+				)}
 			</ul>
 			<button onClick={() => checkout()}>Go to checkout</button>
 			<button onClick={() => console.log(authenticatedUser.shoppingCart[0].products)}>authenticatedUser</button>
