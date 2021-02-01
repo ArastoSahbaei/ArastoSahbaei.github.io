72102baa4d652ce2d1d1baa795aadff012d0445b
arasto <Arasto.Sahbaei@gmail.com>
2021-02-01 12:02:16 +0100

    m

--- client/src/components/cart/ShoppingBagToggler.tsx
+++ client/src/components/cart/ShoppingBagToggler.tsx
@@ -4,6 +4,7 @@ import RoutingPath from '../../routes/RoutingPath'
 import { useContext } from 'react'
 import { UserContext } from '../../shared/provider/UserProvider'
 import APIService from '../../shared/api/service/APIService'
+import emptyCart from '../../shared/images/empty_cart.png'
 
 export const ShoppingBagToggler = (props: { isShoppingBagOpen: boolean, setIsShoppingBagOpen: (handler: boolean) => void }) => {
 	const history = useHistory()
@@ -15,6 +16,24 @@ export const ShoppingBagToggler = (props: { isShoppingBagOpen: boolean, setIsSho
 		setIsShoppingBagOpen(false)
 	}
 
+	const displayEmptyCart = () => {
+		return <img src={emptyCart} alt='' />
+	}
+
+	const displayCartWithItems = () => {
+		return authenticatedUser?.shoppingCart[0]?.products?.map((product: string, index: number) =>
+			<ul key={index}>
+				<li onClick={() => removeProductFromCart(authenticatedUser?.shoppingCart[0]?.products, index)}> {product} </li>
+			</ul>
+		)
+
+	}
+
+	const displayCart = () => {
+		return authenticatedUser.shoppingCart[0].products.length === 0
+			? displayEmptyCart()
+			: displayCartWithItems()
+	}
 
 	const removeProductFromCart = async (array: [], index: number) => {
 		const newArray = [...array.slice(0, index), ...array.slice(index + 1)]
@@ -28,13 +47,7 @@ export const ShoppingBagToggler = (props: { isShoppingBagOpen: boolean, setIsSho
 	return (
 		<div className={isShoppingBagOpen ? 'cart-drawer open' : 'cart-drawer'}>
 			<h1 onClick={() => setIsShoppingBagOpen(false)}>Exit</h1>
-			<ul>
-				{authenticatedUser?.shoppingCart[0]?.products?.map((product: string, index: number) =>
-					<div key={index}>
-						<li onClick={() => removeProductFromCart(authenticatedUser?.shoppingCart[0]?.products, index)}> {product} </li>
-					</div>
-				)}
-			</ul>
+			{displayCart()}
 			<button onClick={() => checkout()}>Go to checkout</button>
 		</div>
 	)
