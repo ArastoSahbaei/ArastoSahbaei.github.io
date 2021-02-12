f25d3416a35d6e778f15b3979c41eb00b228fe08
arasto <Arasto.Sahbaei@gmail.com>
2021-02-12 14:18:36 +0100

    m

--- client/src/view/navigationtabviews/products/categoryfilter/CategoryFilter.tsx
+++ client/src/view/navigationtabviews/products/categoryfilter/CategoryFilter.tsx
@@ -2,14 +2,14 @@ import './CategoryFilter.css'
 
 export const CategoryFilter = () => {
 	return (
-		<div>
-			<p>PRODUKTKATEGORIER</p>
-			<p>Category 1</p>
-			<p>Category 2</p>
-			<p>Category 3</p>
-			<p>Category 4</p>
-			<p>Category 5</p>
-			<p>Category 6</p>
-		</div>
+		<ul className='catergoryFilterWrapper'>
+			<li className='categoryFilterLI'>Alla Produkter</li>
+			<li className='categoryFilterLI'>Category 1</li>
+			<li className='categoryFilterLI'>Category 2</li>
+			<li className='categoryFilterLI'>Category 3</li>
+			<li className='categoryFilterLI'>Category 4</li>
+			<li className='categoryFilterLI'>Category 5</li>
+			<li className='categoryFilterLI'>Category 6</li>
+		</ul>
 	)
 }
