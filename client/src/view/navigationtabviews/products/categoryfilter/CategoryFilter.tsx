b7e4b2ab2da7a42037d28bfaa6f0a892316d3a4f
arasto <Arasto.Sahbaei@gmail.com>
2021-02-12 15:10:43 +0100

    will now display the correct categories

--- client/src/view/navigationtabviews/products/categoryfilter/CategoryFilter.tsx
+++ client/src/view/navigationtabviews/products/categoryfilter/CategoryFilter.tsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from 'react'
+import APIService from '../../../../shared/api/service/APIService'
 import './CategoryFilter.css'
 
 export const CategoryFilter = () => {
+	const [productCategories, setProductCategories] = useState([])
+
+	const fetchData = async () => {
+		const { data } = await APIService.getAllProductCategories()
+		console.log(data)
+		setProductCategories(data)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	productCategories.map((x: any, i: number) => x.productCategoryName)
+
 	return (
 		<ul className='catergoryFilterWrapper'>
 			<li className='categoryFilterLI'>Alla Produkter</li>
-			<li className='categoryFilterLI'>Category 1</li>
-			<li className='categoryFilterLI'>Category 2</li>
-			<li className='categoryFilterLI'>Category 3</li>
-			<li className='categoryFilterLI'>Category 4</li>
-			<li className='categoryFilterLI'>Category 5</li>
-			<li className='categoryFilterLI'>Category 6</li>
-		</ul>
+			{
+				productCategories.map((x: any, i: number) => <li className='categoryFilterLI' key={i}> {x.productCategoryName} </li>)
+			}
+		</ul >
 	)
 }
