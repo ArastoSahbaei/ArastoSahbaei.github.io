d609b7572d91944c5340ac89ab304e067c306c64
arasto <Arasto.Sahbaei@gmail.com>
2021-02-13 21:43:52 +0100

    m

--- client/src/view/userviews/favouriteProductsView.tsx
+++ client/src/view/userviews/favouriteProductsView.tsx
@@ -2,6 +2,7 @@ export const favouriteProductsView = () => {
 	return (
 		<div>
 			<h1>Display the products that user has liked</h1>
+			<p>Vad händer om man tar bort en produkt som en user har i likedProducts?</p>
 		</div>
 	)
 }
